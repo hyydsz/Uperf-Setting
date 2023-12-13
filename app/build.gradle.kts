8c6a86b354fc6356529fd8bb408458097d25f411
珠 水 <lijiyao2007@qq.com>
2023-12-13 16:06:10 +0800

    add

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         targetSdk = 34
 
         versionCode = 2
-        versionName = "1.1"
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
