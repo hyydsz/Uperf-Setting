81924549b047f80d0d8ef5bdfb2ef34fd42e2b88
珠 水 <lijiyao2007@qq.com>
2023-12-13 16:05:00 +0800

    add

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,11 +10,11 @@ android {
 
     defaultConfig {
         applicationId = "com.wateregg.uperfsetting"
-        minSdk = 33
+        minSdk = 26
         targetSdk = 34
 
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- app/src/main/java/com/wateregg/uperfsetting/Data/Data.java
+++ app/src/main/java/com/wateregg/uperfsetting/Data/Data.java
@@ -3,7 +3,7 @@ package com.wateregg.uperfsetting.Data;
 import com.wateregg.uperfsetting.Dialog.SettingDialog;
 
 public class Data {
-    public static SettingDialog.Colors MainThemeName;
-    public static SettingDialog.SliderThemes MainSliderTheme;
+    public static SettingDialog.Colors MainThemeName = SettingDialog.Colors.Blue;
+    public static SettingDialog.SliderThemes MainSliderTheme = SettingDialog.SliderThemes.Normal;
     public static int MainColor = 0;
 }
